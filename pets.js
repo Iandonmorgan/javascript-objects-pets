4e356cb2cd83c7e7a49f19e93ea2870a5d2abcd1
Landon Morgan <morganlandon@gmail.com>
2020-01-19 16:25:11 -0600

    Pet Sounds Practice Complete

--- pets.js
+++ pets.js
@@ -2,5 +2,18 @@ const rory = {
     name: "Aurora Borealis",
     species: "East Nashville Black Dog",
     nicknames: ["Rory", "Ror Bor", "Puppy", "Bunny"],
-    age: 10
+    age: 10,
+    bark: function () {     // Method
+        window.alert("ARF!")
+    },
+    whine: function () {     // Method
+        window.alert("**high pitched dog whine**")
+    },
+    pant: function () {     // Method
+        window.alert("**tongue sticking out, heavy breathing**")
+    }
 };
+
+rory.bark();
+rory.whine();
+rory.pant();
